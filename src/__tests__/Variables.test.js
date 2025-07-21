5a7a6b5ebf51b3fa0608b0a414ad0e80dbfd8d25
JulioBendz <bengutjul2@gmail.com>
2025-07-21 12:49:48 -0500

    Add tests for Variables component to verify title display and variable addition functionality

---
+++ src/__tests__/Variables.test.js
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Variables from '../components/Variables';
+import React from 'react';
+
+test('muestra el título de variables', () => {
+  render(
+    <Variables
+      variables={{}}
+      setVariables={() => {}}
+      setVariableName={() => {}}
+      setVariableValue={() => {}}
+      variableName=""
+      variableValue=""
+    />
+  );
+  expect(screen.getAllByText(/Variables/i).length).toBeGreaterThan(0);
+});
+
+test('permite agregar una variable', () => {
+  const addVariable = jest.fn();
+  render(
+    <Variables
+      variables={{}}
+      addVariable={addVariable}
+      setVariableName={() => {}}
+      setVariableValue={() => {}}
+      variableName="x"
+      variableValue="5"
+      removeVariable={() => {}}
+      editVariable={() => {}}
+    />
+  );
+  fireEvent.click(screen.getByText(/Agregar Variable/i));
+  expect(addVariable).toHaveBeenCalled();
+});
