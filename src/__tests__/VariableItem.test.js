b3b35f169a223b7d70b58f6a5a9d6d19ba5411cc
JulioBendz <bengutjul2@gmail.com>
2025-08-21 16:37:08 -0500

    Add tests for VariableItem component to verify duplication, bulk deletion, copying, and selection functionality

--- src/__tests__/VariableItem.test.js
+++ src/__tests__/VariableItem.test.js
@@ -57,3 +57,171 @@ test('permite editar el valor de la variable', () => {
   fireEvent.click(screen.getByTitle(/Guardar cambios/i));
   expect(onSaveEdit).toHaveBeenCalledWith('x');
 });
+
+test('llama a duplicateVariable al hacer clic en Duplicar', () => {
+  const duplicateVariable = jest.fn();
+  render(
+    <VariableItem
+      name="x"
+      value={10}
+      hoveredItem="x"
+      showDropdown="x"
+      isSelectionMode={false}
+      editingName={null}
+      editingValue=""
+      onMouseEnter={() => {}}
+      onMouseLeave={() => {}}
+      onEdit={() => {}}
+      onSaveEdit={() => {}}
+      onCancelEdit={() => {}}
+      onToggleSelection={() => {}}
+      onCopy={() => {}}
+      onDuplicate={duplicateVariable}
+      onStartDirectDeletion={() => {}}
+      onDropdownToggle={() => {}}
+      onDropdownClose={() => {}}
+      onDeletePanelClose={() => {}}
+      onBulkDelete={() => {}}
+      onSelectAll={() => {}}
+      onDeselectAll={() => {}}
+      selectedVariables={new Set()}
+      currentVariables={[]}
+      startEditing={() => {}}
+      duplicateVariable={duplicateVariable}
+      startDirectDeletion={() => {}}
+      cancelSelectionMode={() => {}}
+      toggleVariableSelection={() => {}}
+      handleBulkDelete={() => {}}
+      deleteControlsPosition={{ name: null, show: false }}
+    />
+  );
+  fireEvent.click(screen.getByText(/Duplicar/i));
+  expect(duplicateVariable).toHaveBeenCalledWith('x', 10);
+});
+
+test('llama a onBulkDelete al hacer clic en Eliminar múltiple', () => {
+  const onBulkDelete = jest.fn();
+  render(
+    <VariableItem
+      name="x"
+      value={10}
+      isSelectionMode={true}
+      isSelected={true}
+      selectedVariables={new Set(['x', 'y'])}
+      currentVariables={[['x', 10], ['y', 20]]}
+      deleteControlsPosition={{ name: 'x', show: true }}
+      onBulkDelete={onBulkDelete}
+      onDeletePanelClose={() => {}}
+      onSelectAll={() => {}}
+      onDeselectAll={() => {}}
+      onMouseEnter={() => {}}
+      onMouseLeave={() => {}}
+      onEdit={() => {}}
+      onSaveEdit={() => {}}
+      onCancelEdit={() => {}}
+      onToggleSelection={() => {}}
+      onCopy={() => {}}
+      onDuplicate={() => {}}
+      onStartDirectDeletion={() => {}}
+      onDropdownToggle={() => {}}
+      onDropdownClose={() => {}}
+      startEditing={() => {}}
+      duplicateVariable={() => {}}
+      startDirectDeletion={() => {}}
+      cancelSelectionMode={() => {}}
+      toggleVariableSelection={() => {}}
+      handleBulkDelete={() => {}}
+      editingName={null}
+      editingValue=""
+      hoveredItem={null}
+      showDropdown={null}
+    />
+  );
+  fireEvent.click(screen.getByText(/Eliminar \(2\)/i));
+  expect(onBulkDelete).toHaveBeenCalled();
+});
+
+test('llama a onCopy al hacer clic en el botón de copiar', () => {
+  const onCopy = jest.fn();
+  render(
+    <VariableItem
+      name="x"
+      value={10}
+      onCopy={onCopy}
+      onMouseEnter={() => {}}
+      onMouseLeave={() => {}}
+      onEdit={() => {}}
+      onSaveEdit={() => {}}
+      onCancelEdit={() => {}}
+      onToggleSelection={() => {}}
+      onDuplicate={() => {}}
+      onStartDirectDeletion={() => {}}
+      onDropdownToggle={() => {}}
+      onDropdownClose={() => {}}
+      onDeletePanelClose={() => {}}
+      onBulkDelete={() => {}}
+      onSelectAll={() => {}}
+      onDeselectAll={() => {}}
+      selectedVariables={new Set()}
+      currentVariables={[]}
+      startEditing={() => {}}
+      duplicateVariable={() => {}}
+      startDirectDeletion={() => {}}
+      cancelSelectionMode={() => {}}
+      toggleVariableSelection={() => {}}
+      handleBulkDelete={() => {}}
+      editingName={null}
+      editingValue=""
+      hoveredItem={null}
+      showDropdown={null}
+      isSelectionMode={false}
+      isSelected={false}
+      deleteControlsPosition={{ name: null, show: false }}
+    />
+  );
+  fireEvent.click(screen.getByTitle(/Copiar variable/i));
+  expect(onCopy).toHaveBeenCalled();
+});
+
+test('permite seleccionar y deseleccionar la variable en modo selección', () => {
+  const onToggleSelection = jest.fn();
+  render(
+    <VariableItem
+      name="x"
+      value={10}
+      isSelectionMode={true}
+      isSelected={false}
+      onToggleSelection={onToggleSelection}
+      selectedVariables={new Set()}
+      currentVariables={[]}
+      deleteControlsPosition={{ name: null, show: false }}
+      onMouseEnter={() => {}}
+      onMouseLeave={() => {}}
+      onEdit={() => {}}
+      onSaveEdit={() => {}}
+      onCancelEdit={() => {}}
+      onCopy={() => {}}
+      onDuplicate={() => {}}
+      onStartDirectDeletion={() => {}}
+      onDropdownToggle={() => {}}
+      onDropdownClose={() => {}}
+      onDeletePanelClose={() => {}}
+      onBulkDelete={() => {}}
+      onSelectAll={() => {}}
+      onDeselectAll={() => {}}
+      startEditing={() => {}}
+      duplicateVariable={() => {}}
+      startDirectDeletion={() => {}}
+      cancelSelectionMode={() => {}}
+      toggleVariableSelection={() => {}}
+      handleBulkDelete={() => {}}
+      editingName={null}
+      editingValue=""
+      hoveredItem={null}
+      showDropdown={null}
+    />
+  );
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+  expect(onToggleSelection).toHaveBeenCalled();
+});
